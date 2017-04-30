b25fcaff2f281af398dbade49ddaa4b8f7927576
lomaxrx <andrewlomaxr@gmail.com>
2017-04-30 18:09:09 -0400

    field rows

--- js/src/components/Person.js
+++ js/src/components/Person.js
@@ -10,54 +10,62 @@ class PersonForm extends Component {
       <section id="contact_information">
         <h2>Contact Information</h2>
         <Form model='person'>
-          <div className='field'>
-            <label>First Name</label>
-            <Control.text model='.first_name'/>
-          </div>
-          <div className='field'>
-            <label>Middle Name</label>
-            <Control.text model='.middle_name'/>
-          </div>
-          <div className='field'>
-            <label>Last Name</label>
-            <Control.text model='.last_name'/>
+          <div className='row'>
+            <div className='field col-md-4'>
+              <label>First Name</label>
+              <Control.text model='.first_name'/>
+            </div>
+            <div className='field col-md-4'>
+              <label>Middle Name</label>
+              <Control.text model='.middle_name'/>
+            </div>
+            <div className='field col-md-4'>
+              <label>Last Name</label>
+              <Control.text model='.last_name'/>
+            </div>
           </div>
           <div className='field'>
             <label>Social Security Number</label>
             <Control.text model='.ssn'/>
           </div>
-          <div className='field'>
-            <label>Date of Birth</label>
-            <DatePicker model='.dob'/>
-          </div>
-          <div className='field'>
-            <label>City of Birth</label>
-            <Control.text model='.city_of_birth'/>
-          </div>
-          <div className='field'>
-            <label>State of Birth</label>
-            <Control.text model='.state_of_birth'/>
-          </div>
-          <div className='field'>
-            <label>Country of Birth</label>
-            <Control.text model='.country_of_birth'/>
-          </div>
-          <div className='field'>
-            <label>Citizenship</label>
-            <Control.text model='.citizenship'/>
-          </div>
-          <div className='field'>
+          <div className='row'>
+            <div className='field col-md-4'>
+              <label>Date of Birth</label>
+              <DatePicker model='.dob'/>
+            </div>
+            <div className='field col-md-6'>
+              <label>City of Birth</label>
+              <Control.text model='.city_of_birth'/>
+            </div>
+            <div className='field col-md-2'>
+              <label>State of Birth</label>
+              <Control.text model='.state_of_birth'/>
+            </div>
+         </div>
+         <div className='row'>
+            <div className='field col-md-6'>
+              <label>Country of Birth</label>
+              <Control.text model='.country_of_birth'/>
+            </div>
+            <div className='field col-md-6'>
+              <label>Citizenship</label>
+              <Control.text model='.citizenship'/>
+            </div>
+        </div>
+        <div className='row'>
+          <div className='field col-md-4'>
             <label>Cell Phone</label>
             <Control.text model='.cell_phone'/>
           </div>
-          <div className='field'>
+          <div className='field col-md-4'>
             <label>Home Phone</label>
             <Control.text model='.home_phone'/>
           </div>
-          <div className='field'>
+          <div className='field col-md-4'>
             <label>Work Phone</label>
             <Control.text model='.work_phone'/>
           </div>
+        </div>
           <div className='field'>
             <label>Email</label>
             <Control.text model='.email'/>
@@ -66,37 +74,43 @@ class PersonForm extends Component {
             <label>Occupation</label>
             <Control.text model='.occupation'/>
           </div>
-          <div className='field'>
-            <label>Marital Status</label>
-            <Control.text model='.marital_status'/>
+          <div className='row'>
+            <div className='field col-md-6'>
+              <label>Marital Status</label>
+              <Control.text model='.marital_status'/>
+            </div>
+            <div className='field col-md-6'>
+              <label>Student Status</label>
+              <Control.text model='.student_status'/>
+            </div>
           </div>
-          <div className='field'>
-            <label>Student Status</label>
-            <Control.text model='.student_status'/>
+          <div className='row'>
+            <div className='field col-md-4'>
+              <label>Gender</label>
+              <Control.text model='.gender'/>
+            </div>
+            <div className='field col-md-4'>
+              <label>Race</label>
+              <Control.text model='.race'/>
+            </div>
+            <div className='field col-md-4'>
+              <label>Ethnicity</label>
+              <Control.text model='.ethnicity'/>
+            </div>
           </div>
-          <div className='field'>
-            <label>Gender</label>
-            <Control.text model='.gender'/>
-          </div>
-          <div className='field'>
-            <label>Race</label>
-            <Control.text model='.race'/>
-          </div>
-          <div className='field'>
-            <label>Ethnicity</label>
-            <Control.text model='.ethnicity'/>
-          </div>
-          <div className='field'>
-            <label>Driver License Number</label>
-            <Control.text model='.driver_license_number'/>
-          </div>
-          <div className='field'>
-            <label>Driver License State</label>
-            <Control.text model='.driver_license_state'/>
-          </div>
-          <div className='field'>
-            <label>Drive License Expiration Date</label>
-            <Control.text model='.drive_licenese_exp_date'/>
+          <div className='row'>
+            <div className='field col-md-2'>
+              <label>Driver License State</label>
+              <Control.text model='.driver_license_state'/>
+            </div>
+            <div className='field col-md-6'>
+              <label>Driver License Number</label>
+              <Control.text model='.driver_license_number'/>
+            </div>
+            <div className='field col-md-4'>
+              <label>Drive License Expiration Date</label>
+              <Control.text model='.drive_licenese_exp_date'/>
+            </div>
           </div>
         </Form>
       </section>
