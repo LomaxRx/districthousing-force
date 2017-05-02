cc9ca2e0e9150c096da98ab63c1e3885a7d8d414
lomaxrx <andrewlomaxr@gmail.com>
2017-05-02 12:49:21 -0400

    minor refactor

--- js/src/components/FormSection.js
+++ js/src/components/FormSection.js
@@ -8,9 +8,9 @@ class FormSection extends Component {
     let el = this.el;
     let { dispatch, id } = this.props;
     let top = el.getBoundingClientRect().top - 10;
-    let bottom = this.el.offsetHeight;
+    let bottom = this.el.offsetHeight + 20;
 
-    if(top < 0 && top > -(bottom+20)){
+    if(top < 0 && top > -bottom){
       dispatch({
         type: 'SET_IN_VIEW_SECTION',
         id: '#'+id
